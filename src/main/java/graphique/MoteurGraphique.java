843f00d0b674db597c98a623c409e682bb813a17
piboons <pauline.disalvocilia@gmail.com>
2023-10-16 17:13:54 +0200

    correction

--- src/main/java/graphique/MoteurGraphique.java
+++ src/main/java/graphique/MoteurGraphique.java
@@ -7,23 +7,16 @@ import java.util.List;
 
 public class MoteurGraphique {
     Frame mainFrame;
-    List<Panel> displayedObjects;
 
-    public void createPanelToDisplay(Objet o){
-
-    }
-    public void removePanelFromDisplay(Objet o){
-
+    public MoteurGraphique(Frame mainFrame) {
+        this.mainFrame = mainFrame;
     }
 
-    public Panel getPanel(Objet o) throws IOException {
-        return new Panel();
-    }
-    public void displayPanel(Panel p){
+    public void displayObject(Object o){ // affiche un objet
 
     }
 
-    public void display(){
+    public void display(List<Object> objects){ // affiche tout
 
     }
 
