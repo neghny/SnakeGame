e4a2c8496640e7701be99620f21f4f0e0233ba0d
ghann <nesrine.ghannay@etu.univ-amu.fr>
2023-10-16 15:26:13 +0200

    Création et implémentation classe Frame

--- src/main/java/graphique/Frame.java
+++ src/main/java/graphique/Frame.java
@@ -1,6 +1,26 @@
 package graphique;
+import javax.swing.*;
 
-public class Frame {
+public class Frame extends JFrame{
+
+    String title;
+    int width;
+    int height;
+    Boolean isVisible;
 
 
+    public Frame(int width, int height, String title, Boolean isVisible) {
+        this.width = width;
+        this.height = height;
+        this.title = title;
+        this.isVisible = isVisible;
+    }
+
+    public void display(){
+        setTitle(this.title);
+        setSize(this.width, this.height);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(this.isVisible);
+    }
 }
