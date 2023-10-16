0692a92639f6f7307ad6ec062660733ca765ba5b
piboons <pauline.disalvocilia@gmail.com>
2023-10-16 15:54:23 +0200

    début de déclaration de méthodes

---
+++ src/main/java/graphique/MoteurGraphique.java
@@ -0,0 +1,20 @@
+package graphique;
+
+import kernel.Objet;
+
+import java.util.List;
+
+public class MoteurGraphique {
+    Frame mainFrame;
+    List<Panel> displayedObjects;
+
+    public void createPanelToDisplay(Objet o){
+
+    }
+    public void removePanelFromDisplay(Objet o){
+
+    }
+    public void displayObject(Objet p){
+
+    }
+}
