8998d5b9bd04bcc165673c064966b5428c2b133c
Kawthar <kawthar.goumaidi@gmail.com>
2023-10-31 09:52:03 +0100

    structure

--- src/main/java/kernel/Gameplay.java
+++ src/main/java/kernel/Gameplay.java
@@ -1,4 +1,18 @@
 package kernel;
 
+import java.util.ArrayList;
+
 public class Gameplay {
+
+    ArrayList<Objet> serpent;
+    Objet pomme;
+
+
+    public Gameplay(Objet pomme) {
+        this.pomme = pomme;
+        this.serpent = new ArrayList<Objet>();
+    }
+
+
+
 }
