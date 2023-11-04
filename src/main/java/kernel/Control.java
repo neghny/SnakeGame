5a71df3991b78dd1d18689d132bf9c7153d6986e
piboons <pauline.disalvocilia@gmail.com>
2023-11-04 09:38:10 +0100

    detection des collisions

--- src/main/java/kernel/Control.java
+++ src/main/java/kernel/Control.java
@@ -25,7 +25,7 @@ public class Control {
         running = true;
         moteurGraphique = new MoteurGraphique(width, height, "Frame");
         gp = new Gameplay(width, height);
-        moteurGraphique.getMainFrame().addKeyListener(new KeyListenerKernel(gp.objets));
+        moteurGraphique.getMainFrame().addKeyListener(new KeyListenerKernel(gp.objets)); // est ce que ici c'est pas gp.sepent plutot ? la pomme s'en fout  du clavier ?
     }
 
     public boolean depassementBords(Objet o){
@@ -43,7 +43,8 @@ public class Control {
 
         while (running) {
             startTime = System.currentTimeMillis();
-            expectedRestart = startTime + 33;
+            //expectedRestart = startTime + 33;
+            expectedRestart = startTime + 100;
 
             /*for (int i = 0; i < gp.objets.size(); i ++) {
                 Objet o1 = gp.objets.get(i);
@@ -72,9 +73,21 @@ public class Control {
                     }
                 }
             }*/
+            Objet teteSerpent = gp.serpent.get(0);
 
-            if (depassementBords(gp.serpent.get(0))) { // la tête du serpent dépasse les bords
+            if (teteSerpent.percute(gp.pomme)){
+                gp.collisionSerpentPomme();
+            }
+            else if (depassementBords(teteSerpent)) { // la tête du serpent dépasse les bords
                 gp.collisionSerpentMur();
+            }
+            else {
+                for (int i = 1; i < gp.serpent.size(); i++) {
+                    //if (teteSerpent.percute(gp.serpent.get(i))){
+                    if (teteSerpent.getXposition() == gp.serpent.get(i).getXposition() && teteSerpent.getYposition() == gp.serpent.get(i).getYposition()) {
+                        gp.collisionSerpent();
+                    }
+                }
             }
 
             if (expectedRestart > System.currentTimeMillis()) {
@@ -104,27 +117,28 @@ public class Control {
 
     public static void main(String[] args) {
         var c = new Control(1000, 1000);
-        Objet pacman = new Objet(25, 25, new Cercle(60), "pacman.png", 120, 120);
-        c.addObjet(pacman);
-        pacman.setSpeed(10);
-        Objet monstre = new Objet(250, 0, new Rectangle(120, 120), "pink_ghost.png", 120, 120);
-        c.addObjet(monstre);
-        c.getMoteurGraphique().init_display(c.getObjets());
+//        Objet pacman = new Objet(25, 25, new Cercle(60), "pacman.png", 120, 120);
+//        c.addObjet(pacman);
+//        pacman.setSpeed(10);
+//        Objet monstre = new Objet(250, 0, new Rectangle(120, 120), "pink_ghost.png", 120, 120);
+//        c.addObjet(monstre);
+//        c.getMoteurGraphique().init_display(c.getObjets());
         /*Objet monstre = new Objet(250, 0, new Rectangle(120, 120), "pink_ghost.png", 120, 120);
         c.addObj(monstre);*/
-        for (int i = 710; i < 1000; i+=50){
-            c.addObjet(new Objet(i, 500, new Cercle(10), "ball.png", 20, 20));
-            c.addObjet(new Objet(i-100, 20, new Cercle(10), "ball.png", 20, 20));
-            c.addObjet(new Objet(100, i-150, new Cercle(10), "ball.png", 20, 20));
-            c.addObjet(new Objet(i-700, 300, new Cercle(10), "ball.png", 20, 20));
-        }
-        Objet monstreRose = new Objet(500, 50, new Rectangle(120, 120), "pink_ghost.png", 120, 120);
-        c.addObjet(monstreRose);
-        Objet monstreBleu = new Objet(800, 300, new Rectangle(120, 120), "blue_ghost.png", 120, 120);
-        c.addObjet(monstreBleu);
-        Objet monstreRouge = new Objet(400, 550, new Rectangle(120, 120), "red_ghost.png", 120, 120);
-        c.addObjet(monstreRouge);
-        c.getMoteurGraphique().init_display(c.getObjets());
+        c.getMoteurGraphique().init_display(c.gp.objets);
+//        for (int i = 710; i < 1000; i+=50){
+//            c.addObjet(new Objet(i, 500, new Cercle(10), "ball.png", 20, 20));
+//            c.addObjet(new Objet(i-100, 20, new Cercle(10), "ball.png", 20, 20));
+//            c.addObjet(new Objet(100, i-150, new Cercle(10), "ball.png", 20, 20));
+//            c.addObjet(new Objet(i-700, 300, new Cercle(10), "ball.png", 20, 20));
+//        }
+//        Objet monstreRose = new Objet(500, 50, new Rectangle(120, 120), "pink_ghost.png", 120, 120);
+//        c.addObjet(monstreRose);
+//        Objet monstreBleu = new Objet(800, 300, new Rectangle(120, 120), "blue_ghost.png", 120, 120);
+//        c.addObjet(monstreBleu);
+//        Objet monstreRouge = new Objet(400, 550, new Rectangle(120, 120), "red_ghost.png", 120, 120);
+//        c.addObjet(monstreRouge);
+//        c.getMoteurGraphique().init_display(c.getObjets());
         c.run();
     }
 }
