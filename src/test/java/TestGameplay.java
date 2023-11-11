4a9a7b1cb6d1f2c9c714f12354e1563febd70e7c
ghann <nesrine.ghannay@etu.univ-amu.fr>
2023-11-11 09:58:56 +0100

    déclaration des test (à implémenter)

---
+++ src/test/java/TestGameplay.java
@@ -0,0 +1,68 @@
+import kernel.Objet;
+import org.junit.jupiter.api.Test;
+import physique.Ligne;
+import physique.Rectangle;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestGameplay {
+
+    /**
+     * Permet de vérfier si le calcul du nouveau placement de la pomme est correcte. Pour cela il faut que :
+     * - La position de la pomme à bien changé
+     * - La pomme n'est pas placé sur le serpent
+     */
+    @Test
+    void testReplacerPomme(){
+
+    }
+
+    /**
+     * Permet de vérifier que lorsqu'il il a collision entre le serpent et une pomme
+     * - Le serpent grandit d'un bloc (le serpent possède un objet de plus suivant les positions du dernier bloc)
+     * - La pomme est replacée
+     */
+    @Test
+    void testCollisionSerpentPomme() {
+
+    }
+
+    /**
+     * Permet de vérfier si un objet a bien été ajouté au serpent:
+     * - le nouvel élément ajouté à la place de la pomme qui est la nouvelle tête du serpent
+     * - l'objet est bien ajouté à la liste d'objets
+     * méthodes testée : addObjetSerpent(), addObj()
+     */
+    @Test
+    void testAddObjetSerpent(){
+
+    }
+
+    /**
+     * test de la fonction depassementBords
+     * On doit vérifier qu'on a bien une détection lorsque le serpent dépasse la fenêtre de jeu
+     */
+    @Test
+    void testDepassementBord(){
+
+
+    }
+
+    /**
+     * Verfication que le score suit bien la suite des évènement
+     */
+    @Test
+    void testScore(){
+
+    }
+
+    /**
+     * Vérification de la réinitialisation du jeu :
+     * - création du nouveau serpent
+     */
+    @Test
+    void testResetLevel(){
+
+    }
+}
